c9159b0ca3445d25fda4a58da0ff654965f6c831
Iradini <mariairadini@gmail.com>
2020-05-17 17:28:14 -0300

    se agrega requirements y se modifica new_search

--- listapp/views.py
+++ listapp/views.py
@@ -1,4 +1,7 @@
+import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+from . import models
 
 # Create your views here.
 def home(request):
@@ -7,4 +10,12 @@ def home(request):
 def new_search(request):
     search = request.POST.get('search')
     models.Search.objects.create(search=search)
-    final_url = BASE_APPLIST_URL.format(quote_plus(search))
+
+    final_postings = []
+
+    stuff_for_frontend = {
+        'search': search,
+        'final_postings': final_postings,
+    }
+
+    return render(request, 'listapp/new_search.html', stuff_for_frontend)
